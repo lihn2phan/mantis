a4c28328c7e73d4c90efd50a2a6856d3bf3e8d90
lihn2phan <angelina.lihn.brazhnikova@gmail.com>
2023-06-04 09:48:49 +0300

    Сделан тест для регистрации нового пользователя в mantis через почту

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
 from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 class Application:
     def __init__(self, browser, config):
         if browser == "firefox":
@@ -16,6 +18,8 @@ class Application:
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
         self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
         self.config = config
         self.base_url = config['web']['baseUrl']
 
